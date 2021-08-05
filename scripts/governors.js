eaf141852032a31c9a9779cbc0d0b6ed9a746250
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-08-05 11:37:04 -0500

    governors dropdown list implemented

---
+++ scripts/governors.js
@@ -0,0 +1,72 @@
+import { getGovernors, setGovernor } from "./database.js";
+
+const governors = getGovernors()
+
+
+
+
+
+
+
+
+
+
+// ---- Dropdown List For Governor Selection ---- \\
+
+
+export const Governors = () => {
+    let html = "<ul>"
+        
+            
+                
+                
+                const listItems = governors.map(governor =>  {
+                    return `<ul>
+                    
+                    <option value="${governor.id}">${governor.name}</option>
+                    
+                    </ul> `
+                })    
+                
+                
+                
+                
+                
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
+
+
+
+
+
+
+
+
+
+
+        `
+            <label for="governors" >Choose Governor</label>
+
+                <select name="governors" id="governors">
+                    <option value="${governors.id}">${governors.name}</option>
+                </select>
+               
+                `   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
